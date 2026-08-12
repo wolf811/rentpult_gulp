5e09b943bb4d84e89d0fd878b0a92c74e5e5b080
wolf811 <wolf811@gmail.com>
2026-08-12 15:25:33 +0300

    add contacts

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import './_components';
 
 const elements = document.querySelectorAll('.btn-group-action');
 
-window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function (e) {
   // e.preventDefault();
   if (window.innerWidth <= 768) {
     for (let el of elements) {
@@ -23,3 +23,32 @@ window.addEventListener('resize', function(e) {
   }
 });
 
+// document.querySelectorAll('input[name="toogleCountriesLanguages"]').forEach(radio => {
+//   radio.addEventListener('change', function () {
+//     const countries = document.getElementById('countries');
+//     const languages = document.getElementById('languages');
+
+//     if (this.value === '1') {
+//       countries.classList.remove('d-none');
+//       languages.classList.add('d-none');
+//     } else {
+//       languages.classList.remove('d-none');
+//       countries.classList.add('d-none');
+//     }
+//   });
+// });
+
+document.querySelectorAll('input[name="toogleCountriesLanguages"]').forEach(radio => {
+  radio.addEventListener('change', function () {
+    const countries = document.getElementById('countries');
+    const languages = document.getElementById('languages');
+
+    if (this.value === '1') {
+      countries.classList.remove('d-none');
+      languages.classList.add('d-none');
+    } else {
+      languages.classList.remove('d-none');
+      countries.classList.add('d-none');
+    }
+  });
+});
